56b397d9822c7ead985c56ba0a5b485b9ccc3a08
jayunruh <jru@stowers.org>
2016-03-21 08:51:26 -0500

    update

--- measure_trajectories_jru_v1.java
+++ measure_trajectories_jru_v1.java
@@ -22,46 +22,71 @@ public class measure_trajectories_jru_v1 implements PlugIn {
 		GenericDialog gd=new GenericDialog("Options");
 		gd.addChoice("Statistic",jstatistics.stats,"Avg");
 		gd.addNumericField("Measure_Radius",5.0,5,15,null);
+		gd.addNumericField("Measure_Radius_Z (optional)",5.0,5,15,null);
 		gd.showDialog(); if(gd.wasCanceled()){return;}
 		String stat=jstatistics.stats[gd.getNextChoiceIndex()];
 		float rad=(float)gd.getNextNumber();
+		float zrad=(float)gd.getNextNumber();
 		ImageWindow iw=imps[1].getWindow();
 		float[][] xvals=(float[][])jutils.runPW4VoidMethod(iw,"getXValues");
 		float[][] yvals=(float[][])jutils.runPW4VoidMethod(iw,"getYValues");
-		int[] npts=(int[])jutils.runPW4VoidMethod(iw,"getNpts");
 		String[] starts=(String[])jutils.runPW4VoidMethod(iw,"getAnnotations");
+		boolean threed=jutils.is3DPlot(iw);
+		int[] npts=null;
+		if(threed) npts=((int[][])jutils.runPW4VoidMethod(iw,"getNpts"))[0];
+		else npts=(int[])jutils.runPW4VoidMethod(iw,"getNpts");
+		float[][] zvals=null;
+		if(threed) zvals=((float[][][])jutils.runPW4VoidMethod(iw,"getZValues"))[0];
 
 		ImageStack stack=imps[0].getStack();
 		int width=imps[0].getWidth(); int height=imps[0].getHeight();
-		int frames=stack.getSize(); //assume single channel for now
+		int channels=imps[0].getNChannels();
+		int currchan=imps[0].getC();
+		int slices=imps[0].getNSlices();
+		int frames=imps[0].getNFrames();
+		if(threed && slices==1){slices=frames; frames=1;}
+		//int frames=stack.getSize(); //assume single channel for now
 		//IJ.log(""+npts.length);
 		int totlength=0;
 		for(int i=0;i<npts.length;i++) totlength+=npts[i];
 		float[][] stats=new float[totlength][6];
+		if(threed) stats=new float[totlength][7];
 		int counter=0;
-		//stats are id,start,frame,x,y,stat
+		//stats are id,start,frame,x,y,(z),stat
 		for(int i=0;i<npts.length;i++){
 			int start=0;
 			if(starts!=null){
 				start=(int)Float.parseFloat(starts[i]);
 			}
 			for(int j=start;j<(start+npts[i]);j++){
-				float[] frame=algutils.convert_arr_float2(stack.getPixels(j+1));
-				float[] circ=getCircleVals(frame,width,height,xvals[i][j-start],yvals[i][j-start],rad);
+				float[] circ=null;
+				if(threed){
+					Object[] frame=jutils.get3DZSeries(stack,currchan-1,j,frames,slices,channels);
+					circ=getSphereVals(frame,width,height,xvals[i][j-start],yvals[i][j-start],zvals[i][j-start],rad,zrad);
+				} else { 
+					Object frame=jutils.get3DSlice(stack,j,0,currchan-1,frames,slices,channels);
+					circ=getCircleVals(frame,width,height,xvals[i][j-start],yvals[i][j-start],rad);
+				}
 				stats[counter][0]=i;
 				stats[counter][1]=start;
 				stats[counter][2]=j-start;
 				stats[counter][3]=xvals[i][j-start];
 				stats[counter][4]=yvals[i][j-start];
-				stats[counter][5]=jstatistics.getstatistic(stat,circ,null);
+				if(threed){
+					stats[counter][5]=zvals[i][j-start];
+					stats[counter][6]=jstatistics.getstatistic(stat,circ,null);
+				} else {
+					stats[counter][5]=jstatistics.getstatistic(stat,circ,null);
+				}
 				counter++;
 			}
 		}
 		//now output the stats
-		table_tools.create_table("Traj Stats",stats,new String[]{"id","start","frame","x","y",stat});
+		if(threed) table_tools.create_table("Traj Stats",stats,new String[]{"id","start","frame","x","y","z",stat});
+		else table_tools.create_table("Traj Stats",stats,new String[]{"id","start","frame","x","y",stat});
 	}
 
-	public float[] getCircleVals(float[] image,int width,int height,float xc,float yc,float rad){
+	public float[] getCircleVals(Object image,int width,int height,float xc,float yc,float rad){
 		int size=(int)(2.0f*rad+1.0f);
 		float[] square=new float[size*size];
 		int startx=(int)(xc-rad);
@@ -73,8 +98,43 @@ public class measure_trajectories_jru_v1 implements PlugIn {
 				float x=xc-(float)j;
 				float dist2=x*x+y*y;
 				if(dist2<=rad && i>=0 && i<height && j>=0 && j<width){
-					square[counter]=image[j+i*width];
+					int pos=j+i*width;
+					Object temp=image;
+					if(temp instanceof float[]) square[counter]=((float[])temp)[pos];
+					if(temp instanceof short[]) square[counter]=(float)(((short[])temp)[pos]&0xffff);
+					if(temp instanceof byte[]) square[counter]=(float)(((byte[])temp)[pos]&0xff);
 					counter++;
+				}
+			}
+		}
+		return (float[])algutils.get_subarray(square,0,counter);		
+	}
+
+	public float[] getSphereVals(Object[] image,int width,int height,float xc,float yc,float zc,float rad,float zrad){
+		int size=(int)(2.0f*rad+1.0f);
+		int zsize=(int)(2.0f*zrad+1.0f);
+		int slices=image.length;
+		float[] square=new float[size*size*zsize];
+		int startx=(int)(xc-rad);
+		int starty=(int)(yc-rad);
+		int startz=(int)(zc-zrad);
+		float zratio=zrad/rad;
+		int counter=0;
+		for(int i=starty;i<(starty+size);i++){
+			float y=yc-(float)i;
+			for(int j=startx;j<(startx+size);j++){
+				float x=xc-(float)j;
+				for(int k=startz;k<(startz+zsize);k++){
+					float z=zc-(float)k;
+					float dist2=x*x+y*y+z*z/(zratio*zratio);
+					if(dist2<=rad && i>=0 && i<height && j>=0 && j<width && k>=0 && k<slices){
+						Object temp=image[k];
+						int pos=j+i*width;
+						if(temp instanceof float[]) square[counter]=((float[])temp)[pos];
+						if(temp instanceof short[]) square[counter]=(float)(((short[])temp)[pos]&0xffff);
+						if(temp instanceof byte[]) square[counter]=(float)(((byte[])temp)[pos]&0xff);
+						counter++;
+					}
 				}
 			}
 		}
