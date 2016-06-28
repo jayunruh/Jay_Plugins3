5de5ce2640c72f54f4e6ded8d764a2922be55fc2
jayunruh <jru@stowers.org>
2016-06-28 16:35:59 -0500

    update

---
+++ jay_stitching_plugin_jru_v1.java
@@ -0,0 +1,169 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.Frame;
+import ij.plugin.*;
+import jalgs.*;
+import jguis.*;
+
+public class jay_stitching_plugin_jru_v1 implements PlugIn, gui_interface {
+
+	public void run(String arg) {
+		ImagePlus imp = IJ.getImage();
+		int width=imp.getWidth(); int height=imp.getHeight();
+		ImageStack stack=imp.getStack();
+		int slices=stack.getSize();
+		GenericDialog gd=new GenericDialog("Options");
+		gd.addNumericField("Overlap Percent",20.0,5,15,null);
+		gd.addNumericField("X Images",3,0);
+		gd.addNumericField("Y Images",3,0);
+		gd.addCheckbox("Output_Coords",true);
+		String[] scantype={"Snake_by_rows","Raster_by_rows"};
+		gd.addChoice("Scan_Type",scantype,scantype[0]);
+		String[] startoptions={"Upper_Left","Upper_Right","Lower_Left","Lower_Right"};
+		gd.addChoice("Scan_Start",startoptions,startoptions[0]);
+		gd.addNumericField("Median_Shift_Tolerance (%)",10,5,15,null);
+		gd.addNumericField("Correlation_Threshold",0.3,5,15,null);
+		gd.addNumericField("Number of threads",4,0);
+		gd.showDialog(); if(gd.wasCanceled()){return;}
+		float overlap=(float)gd.getNextNumber()/100.0f;
+		int ximgs=(int)gd.getNextNumber();
+		int yimgs=(int)gd.getNextNumber();
+		boolean outcoords=gd.getNextBoolean();
+		int scantypeoption=gd.getNextChoiceIndex();
+		int scandiroption=gd.getNextChoiceIndex();
+		float medshifttoler=(float)gd.getNextNumber()/100.0f;
+		float corrthresh=(float)gd.getNextNumber();
+		int nthreads=(int)gd.getNextNumber();
+		int xstart=0; int ystart=yimgs-1; int xscandir=1; int yscandir=-1;
+		if(scandiroption==0){ystart=0; yscandir=1;}
+		if(scandiroption==1){xstart=ximgs-1; xscandir=-1; ystart=0; yscandir=1;}
+		if(scandiroption==3){xstart=ximgs-1; xscandir=-1;}
+		float[][] coords=null;
+		int[][] pairs=null;
+		if(scantypeoption==0){
+			coords=stitching.getTileCoords(ximgs,yimgs,width,height,overlap,xstart,ystart,xscandir,yscandir,1.0f);
+			pairs=stitching.getPairs(ximgs,yimgs,xscandir,yscandir,false);
+		} else{
+			coords=stitching.getTileCoordsRaster(ximgs,yimgs,width,height,overlap,xstart,ystart,xscandir,yscandir,1.0f);
+			pairs=stitching.getPairsRaster(ximgs,yimgs,xscandir,yscandir,false);
+		}
+		PlotWindow4 coordplot=null;
+		if(outcoords){
+			coordplot=new PlotWindow4("Coordinates","x","y",coords[0],coords[1]);
+			coordplot.draw();
+		}
+		stitching s=new stitching(width,height,coords[0],coords[1],algutils.get_array_type(stack.getPixels(1)),this);
+
+		ImageStack ccstack=null;
+		float[][] stats=new float[pairs.length][8];
+		float[][] pairStats=new float[pairs.length][4];
+		Object[] pix=jutils.stack2array(stack);
+		float[][] cc=s.phaseCorrPad(pix,pairs,nthreads); //this is multithreaded
+		for(int i=0;i<pairs.length;i++){
+			int pair1=pairs[i][0]; int pair2=pairs[i][1];
+			float predxshift=coords[0][pair2]-coords[0][pair1];
+    			float predyshift=coords[1][pair2]-coords[1][pair1];
+			stats[i][0]=(float)pair1; stats[i][1]=(float)pair2;
+			stats[i][2]=predxshift; stats[i][3]=predyshift;
+			if(ccstack==null) ccstack=new ImageStack(s.fftdim,s.fftdim);
+			ccstack.addSlice(""+pair1+","+pair2,cc[i]);
+			float[][] peaks=s.getCorrPeaks(cc[i],5,pix[pair1],pix[pair2],new float[]{predxshift,predyshift});
+			//peaks are ranked in order of decreasing correlation
+			//float[] bestpeak=findClosestPeak(peaks,new float[]{predxshift,predyshift});
+			float[] bestpeak=peaks[0];
+			for(int j=0;j<peaks.length;j++){
+				IJ.log(""+peaks[j][4]+" , "+peaks[j][0]+" , "+peaks[j][1]+" , "+peaks[j][2]);
+			}
+			pairStats[i][0]=bestpeak[1]; pairStats[i][1]=bestpeak[2]; pairStats[i][2]=bestpeak[0]; pairStats[i][3]=bestpeak[4];
+			stats[i][4]=pairStats[i][0]; stats[i][5]=pairStats[i][1];
+			stats[i][6]=pairStats[i][2]; stats[i][7]=pairStats[i][3];
+			IJ.log(""+pair1+ " <- "+pair2+":,"+stats[i][2]+" , "+stats[i][3]+" , "+stats[i][4]+ " , "+stats[i][5]+" , " + " correlation (R)=" + stats[i][6]+" , "+stats[i][7]);
+		}
+		//new ImagePlus("Norm Correlation Images",ccstack).show();
+		//check the coordinates
+		//all vertical and horizontal offsets should be within ftoler (10%) of their median positive values
+		//if not, set them to the median and zero their correlation values (they are now slave to the other offsets)
+		float[] medshifts=s.checkCoords(pairStats,pairs,medshifttoler);
+		//find the first pair with significant correlation
+		int fixedindex=0;
+		for(int i=0;i<pairs.length;i++){
+			if(pairStats[i][3]>corrthresh){
+				fixedindex=pairs[i][0];
+				break;
+			}
+		}
+		//now place them on the grid with the fixed image as 0,0
+		float[][] newcoords=stitching.preAlign(pairStats,fixedindex,pairs,ximgs*yimgs,corrthresh);
+		float[] newxvals=new float[newcoords.length];
+		float[] newyvals=new float[newcoords.length];
+		float xmin=newcoords[0][0]; float ymin=newcoords[0][1];
+		for(int i=0;i<newcoords.length;i++){
+			newxvals[i]=newcoords[i][0];
+			newyvals[i]=newcoords[i][1];
+			if(newxvals[i]<xmin) xmin=newxvals[i];
+			if(newyvals[i]<ymin) ymin=newyvals[i];
+		}
+		for(int i=0;i<newcoords.length;i++){
+			newxvals[i]-=xmin; newyvals[i]-=ymin;
+		}
+		s.setCoords(newxvals,newyvals,1.0f);
+		if(outcoords) coordplot.addPoints(newxvals,newyvals,true);
+		//float hoverlap=(float)width-Math.abs(medshifts[0]);
+		//float voverlap=(float)height-Math.abs(medshifts[3]);
+		float[] overlaps=getAvgOverlap(new float[][]{newxvals,newyvals},width,height);
+		float hoverlap=overlaps[0]; float voverlap=overlaps[1];
+		IJ.log("overlaps: "+hoverlap+" , "+voverlap);
+		Object stitched=s.stitch_frame(jutils.stack2array(stack),true,hoverlap,voverlap);
+		ImageStack outstack=new ImageStack(s.newwidth,s.newheight);
+		outstack.addSlice("",stitched);
+		new ImagePlus("Stitched Image",outstack).show();
+	}
+
+	public static float[] getAvgOverlap(float[][] coords,int width,int height){
+		float hover=0.0f; int nhpairs=0;
+		float vover=0.0f; int nvpairs=0;
+		float halfwidth=0.5f*(float)width;
+		float halfheight=0.5f*(float)height;
+		for(int i=0;i<coords[0].length;i++){
+			for(int j=(i+1);j<coords[0].length;j++){
+				float xdist=Math.abs(coords[0][j]-coords[0][i]);
+				float ydist=Math.abs(coords[1][j]-coords[1][i]);
+				if(xdist>halfwidth && xdist<(float)width && ydist<halfheight){
+					hover+=xdist; nhpairs++;
+				}
+				if(xdist<halfwidth && ydist>halfheight && ydist<(float)height){
+					vover+=ydist; nvpairs++;
+				}
+			}
+		}
+		hover/=(float)nhpairs;
+		vover/=(float)nvpairs;
+		return new float[]{(float)width-hover,(float)height-vover};
+	}
+
+	public float[] findClosestPeak(float[][] peaks,float[] shift){
+		float mindist=getdist(new float[]{peaks[0][1],peaks[0][2]},shift);
+		int minindex=0;
+		for(int i=1;i<peaks.length;i++){
+			float dist=getdist(new float[]{peaks[i][1],peaks[i][2]},shift);
+			if(dist<mindist){
+				mindist=dist; minindex=i;
+			}
+		}
+		return peaks[minindex];
+	}
+
+	public float getdist(float[] coords1,float[] coords2){
+		return (float)Math.sqrt((coords1[0]-coords2[0])*(coords1[0]-coords2[0])+(coords1[1]-coords2[1])*(coords1[1]-coords2[1]));
+	}
+
+	public void showMessage(String message){
+		IJ.log(message);
+	}
+
+	public void showProgress(int currpos,int finalpos){
+		IJ.showProgress(currpos,finalpos);
+	}
+
+}
---
+++ stitch_mosaic_image_jru_v1.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Jay Unruh, Stowers Institute for Medical Research.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.*;
+import ij.plugin.frame.*;
+import jguis.*;
+import jalgs.*;
+
+public class stitch_mosaic_image_jru_v1 implements PlugIn, FrameInterface, gui_interface {
+	boolean showrois;
+	int tempchan,tempslice,tempframe,slices,channels,frames;
+	ImageStack tempstack;
+
+	public void run(String arg) {
+		ImagePlus imp=WindowManager.getCurrentImage();
+		ImageWindow[] iws=jutils.selectPlots(false,1,new String[]{"Position_Plot"});
+		if(iws==null) return;
+		Object[] retvals=exec(imp,iws[0]);
+		((ImagePlus)retvals[0]).show();
+	}
+
+	public static float[] getAvgOverlap(float[][] coords,int width,int height){
+		float hover=0.0f; int nhpairs=0;
+		float vover=0.0f; int nvpairs=0;
+		float halfwidth=0.5f*(float)width;
+		float halfheight=0.5f*(float)height;
+		for(int i=0;i<coords[0].length;i++){
+			for(int j=(i+1);j<coords[0].length;j++){
+				float xdist=Math.abs(coords[0][j]-coords[0][i]);
+				float ydist=Math.abs(coords[1][j]-coords[1][i]);
+				if(xdist>halfwidth && xdist<(float)width && ydist<halfheight){
+					hover+=xdist; nhpairs++;
+				}
+				if(xdist<halfwidth && ydist>halfheight && ydist<(float)height){
+					vover+=ydist; nvpairs++;
+				}
+			}
+		}
+		hover/=(float)nhpairs;
+		vover/=(float)nvpairs;
+		return new float[]{(float)width-hover,(float)height-vover};
+	}
+
+	public Object[] exec(ImagePlus imp,ImageWindow iw){
+		float[][] xvals=(float[][])jutils.runPW4VoidMethod(iw,"getXValues");
+		float[][] yvals=(float[][])jutils.runPW4VoidMethod(iw,"getYValues");
+		int sel=(Integer)jutils.runPW4VoidMethod(iw,"getSelected");
+		float[] overlap=getAvgOverlap(new float[][]{xvals[sel],yvals[sel]},imp.getWidth(),imp.getHeight());
+		IJ.log("avg overlap = "+overlap[0]+" , "+overlap[1]);
+		return exec(imp,xvals[sel],yvals[sel],overlap[0],overlap[1]);
+	}
+
+	public Object[] exec(ImagePlus imp,float[] xvals,float[] yvals,float hoverlap,float voverlap){
+		int width=imp.getWidth(); int height=imp.getHeight();
+		ImageStack stack=imp.getStack();
+		int size=stack.getSize();
+		channels=imp.getNChannels();
+		slices=imp.getNSlices();
+		frames=imp.getNFrames();
+		if(frames==1){
+			frames=slices; slices=1;
+		}
+		float psize=(float)jutils.get_psize(imp);
+		int typeindex=algutils.get_array_type(stack.getPixels(1));
+		stitching sclass=new stitching(width,height,xvals,yvals,psize,typeindex,this);
+		ImageStack stack2=new ImageStack(sclass.newwidth,sclass.newheight);
+		tempstack=stack;
+		for(int i=0;i<slices;i++){
+			for(int j=0;j<channels;j++){
+				tempchan=j;
+				tempslice=i;
+				tempframe=0;
+				//Object[] tseries=jutils.get3DTSeries(stack,i,j,frames,slices,channels);
+				//Object stitched=sclass.stitch_frame(tseries,true,overlap);
+				Object stitched=sclass.stitch_frame(this,frames,true,hoverlap,voverlap);
+				stack2.addSlice("",stitched);
+			}
+		}
+		if(showrois){
+			int[][] rois=sclass.getRois();
+			RoiManager rman=RoiManager.getInstance();
+			if(rman==null) rman=new RoiManager();
+			for(int i=0;i<rois.length;i++){
+				rman.addRoi(new Roi(rois[i][0],rois[i][1],rois[i][2],rois[i][3]));
+			}
+		}
+		ImagePlus imp5=new ImagePlus("Stitched Image",stack2);
+		imp5.copyScale(imp);
+		imp5.setOpenAsHyperStack(true);
+		imp5.setDimensions(channels,slices,1);
+		//imp5.show();
+		return new Object[]{imp5};
+	}
+
+	public Object getNextFrame(){
+		Object temp=jutils.get3DSlice(tempstack,tempframe,tempslice,tempchan,frames,slices,channels);
+		tempframe++;
+		return temp;
+	}
+
+	public void showMessage(String message){
+		IJ.log(message);
+	}
+	
+	public void showProgress(int currpos,int finalpos){
+		IJ.showProgress(currpos,finalpos);
+	}
+
+}
