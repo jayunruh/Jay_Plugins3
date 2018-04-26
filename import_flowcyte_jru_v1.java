70fa31eb360a0f153fca361ce5cbb24c0eb17242
jayunruh <jru@stowers.org>
2018-04-26 10:46:23 -0500

    update

--- import_flowcyte_jru_v1.java
+++ import_flowcyte_jru_v1.java
@@ -21,7 +21,7 @@ public class import_flowcyte_jru_v1 implements PlugIn {
 
 	public void run(String arg) {
 		//start by reading the header
-		jdataio jdio=new jdataio();
+		//jdataio jdio=new jdataio();
 		OpenDialog od = new OpenDialog("Open File",arg);
 		String directory=od.getDirectory();
 		String name=od.getFileName();
@@ -30,108 +30,20 @@ public class import_flowcyte_jru_v1 implements PlugIn {
 		gd.addCheckbox("Show Metadata?",false);
 		gd.showDialog(); if(gd.wasCanceled()){return;}
 		boolean showmetadata=gd.getNextBoolean();
-		try{
-			//first read the header
-			InputStream instream=new BufferedInputStream(new FileInputStream(directory+name));
-			String label=jdio.readstring(instream,6);
-			jdio.skipstreambytes(instream,4);
-			String toff=jdio.readstring(instream,8); int textoff=Integer.parseInt(toff.trim()); //start of TEXT segment
-			String teoff=jdio.readstring(instream,8); int texteoff=Integer.parseInt(teoff.trim()); //end of TEXT segment
-			String doff=jdio.readstring(instream,8); int dataoff=Integer.parseInt(doff.trim()); //start of DATA segment
-			String deoff=jdio.readstring(instream,8); int dataeoff=Integer.parseInt(deoff.trim()); //end of DATA segment
-			String aoff=jdio.readstring(instream,8); int analoff=Integer.parseInt(aoff.trim()); //start of ANALYSIS segment
-			String aeoff=jdio.readstring(instream,8); int analeoff=Integer.parseInt(aeoff.trim()); //end of ANALYSIS segment
-			//IJ.log(""+textoff+" , "+texteoff+" , "+dataoff+" , "+dataeoff+" , "+analoff+" , "+analeoff);
-			instream.close();
-			instream=new BufferedInputStream(new FileInputStream(directory+name));
-			jdio.skipstreambytes(instream,textoff);
-			//now read all parameters and values
-			int textlength=texteoff-textoff;
-			String text=jdio.readstring(instream,textlength);
-			jdio.skipstreambytes(instream,1);
-			text=text.replace('\\','/');
-			text=text.replace('|','/');
-			text=text.replace("\u000C","/");
-			text=text.replace("\u001E","/");
-			text=text.replace("*","/");
-			String[] params=text.split("/\\u0024");
-			//String[] params=text.split("/\u0c24");
-			//IJ.log(text);
-			String[] labels=new String[params.length-1];
-			String[] values=new String[params.length-1];
-			for(int i=1;i<params.length;i++){
-				String[] temp=params[i].split("/");
-				labels[i-1]=temp[0]; values[i-1]=temp[1];
-				if(showmetadata){
-					IJ.log(labels[i-1]+" , "+values[i-1]);
+		//Object[] data=getFCSFile(directory+name);
+		Object[] data=(new import_flowcyte()).getFCSFile(directory+name);
+		if(data!=null){
+			if(showmetadata){
+				String[][] meta=(String[][])data[2];
+				for(int i=0;i<meta[0].length;i++){
+					IJ.log(meta[0][i]+" , "+meta[1][i]);
 				}
 			}
-			//now get the number of data points and number of channels
-			int npts=get_label_number(labels,values,"TOT");
-			int nch=get_label_number(labels,values,"PAR");
-			String byteord=get_label_value(labels,values,"BYTEORD");
-			//IJ.log(byteord);
-			boolean motorola=true;
-			if(byteord.startsWith("1,")) motorola=false;
-			//now go through each channel getting its name, bits, and range
-			String[] chnames=new String[nch];
-			int[] chbits=new int[nch];
-			int[] range=new int[nch];
-			for(int i=0;i<nch;i++){
-				chnames[i]=get_label_value(labels,values,"P"+(i+1)+"N");
-				chbits[i]=get_label_number(labels,values,"P"+(i+1)+"B");
-				range[i]=get_label_number(labels,values,"P"+(i+1)+"R");
-			}
+			String[] chnames=(String[])data[0];
 			String headings=table_tools.print_string_array(chnames);
 			TextWindow tw=new TextWindow(name,headings,"",400,200);
-			float[][] temp=new float[npts][nch];
-			instream.close();
-			instream=new BufferedInputStream(new FileInputStream(directory+name));
-			jdio.skipstreambytes(instream,dataoff);
-			for(int i=0;i<npts;i++){
-				for(int j=0;j<nch;j++){
-					if(chbits[j]==16){
-						if(!motorola) temp[i][j]=(float)jdio.readintelshort(instream);
-						else temp[i][j]=(float)jdio.readmotorolashort(instream);
-					} else {
-						if(chbits[j]==32){
-							if(!motorola) temp[i][j]=(float)jdio.readintelfloat(instream);
-							else temp[i][j]=(float)jdio.readmotorolafloat(instream);
-						} else {
-							if(chbits[j]==8){
-								temp[i][j]=(float)jdio.readintelbyte(instream);
-							}
-						}
-					}
-				}
-			}
-			String data=table_tools.print_float_array(temp);
-			tw.append(data);
-			instream.close();
-		} catch(IOException e){
-			IJ.log(e.getMessage());
+			if(data[1]!=null) tw.append(table_tools.print_float_array((float[][])data[1]));
 		}
-	}
-
-	private String get_label_value(String[] labels,String[] values,String match){
-		for(int i=0;i<labels.length;i++){
-			if(labels[i].equals(match)){
-				return values[i];
-			}
-		}
-		return null;
-	}
-
-	private int get_label_number(String[] labels,String[] values,String match){
-		String value=get_label_value(labels,values,match);
-		if(value!=null){
-			try{
-				return Integer.parseInt(value.trim());
-			}catch(NumberFormatException e){
-				return -1;
-			}
-		}
-		return -1;
 	}
 
 }
--- simulate_npcs_jru_v1.java
+++ simulate_npcs_jru_v1.java
@@ -14,56 +14,104 @@ import jalgs.*;
 import jguis.*;
 import jalgs.jfit.*;
 import jalgs.jsim.*;
+import jalgs.jseg.*;
+import ij.text.*;
 
 public class simulate_npcs_jru_v1 implements PlugIn {
 	//this plugin simulates random points on the surface of a sphere convolved with a 3D psf
 	//the image is 128 x 128 x 128
+	public rngs random;
 
 	public void run(String arg) {
 		GenericDialog gd=new GenericDialog("Options");
 		gd.addNumericField("Sphere_Radius (nm)",1000.0,5,15,null);
-		gd.addNumericField("Number_of_points",50,0);
-		gd.addNumericField("PSF_FWHM (nm)",200.0,5,15,null);
-		gd.addNumericField("PSF_Z_FWHM (nm)",600.0,5,15,null);
+		gd.addNumericField("Number_of_points",100,0);
+		gd.addNumericField("PSF_FWHM (nm)",100.0,5,15,null);
+		gd.addNumericField("PSF_Z_FWHM (nm)",300.0,5,15,null);
 		gd.addNumericField("Pixel_Size (nm)",40.0,5,15,null);
+		gd.addNumericField("Max_Intensity (photons)",100.0,5,15,null);
+		gd.addCheckbox("Add_Noise",false);
+		gd.addNumericField("Read_Noise_Stdev",40.0f,5,15,null);
+		gd.addNumericField("Gain (mutiplier)",50.0f,5,15,null);
+		gd.addCheckbox("Add_Spb (2nd channel)",false);
+		gd.addNumericField("Spb_Max_Intensity (photons)",100.0,5,15,null);
+		gd.addNumericField("Spb_Separation (nm)",180.0f,5,15,null);
 		gd.showDialog(); if(gd.wasCanceled()) return;
 		float srad=(float)gd.getNextNumber();
 		int npts=(int)gd.getNextNumber();
 		float xystdev=((float)gd.getNextNumber())/2.35f;
 		float zstdev=((float)gd.getNextNumber())/2.35f;
 		float psize=(float)gd.getNextNumber();
+		float amp=(float)gd.getNextNumber();
+		boolean addnoise=gd.getNextBoolean();
+		float readstdev=(float)gd.getNextNumber();
+		float gain=(float)gd.getNextNumber();
+		boolean addspb=gd.getNextBoolean();
+		float spbamp=(float)gd.getNextNumber();
+		float spbsep=(float)gd.getNextNumber();
+		random=new rngs();
 		float[][] spherepts=makeSphere(npts,srad,64.0f*psize,64.0f*psize,64.0f*psize);
-		plotPoints(spherepts);
+		plotPoints("Npcs",spherepts);
 		Object[] stack=new Object[128];
-		for(int i=0;i<128;i++){
-			stack[i]=new float[128*128];
+		for(int i=0;i<128;i++) stack[i]=new float[128*128];
+		drawPoints(stack,spherepts,psize,psize,xystdev,zstdev,128,128,amp);
+		if(addnoise) addNoise(stack,readstdev,gain);
+		if(addspb){
+			Object[] spbstack=new Object[128];
+			for(int i=0;i<128;i++) spbstack[i]=new float[128*128];
+			float[][] spbpts=makeSpb(spbsep,srad,64.0f*psize,64.0f*psize,64.0f*psize);
+			plotPoints("SPB",spbpts);
+			drawPoints(spbstack,spbpts,psize,psize,xystdev,zstdev,128,128,spbamp);
+			if(addnoise) addNoise(spbstack,readstdev,gain);
+			//now interleave the stacks
+			Object[] stack2=new Object[256];
+			for(int i=0;i<128;i++){
+				stack2[2*i]=stack[i];
+				stack2[2*i+1]=spbstack[i];
+			}
+			ImagePlus imp=jutils.create_hyperstack("Sim NPCs",jutils.array2stack(stack2,128,128),1,128,2,true,null);
+			jutils.set_psize(imp,psize/1000.0f);
+			imp.show();
+		} else {
+			ImagePlus imp=new ImagePlus("Sim NPCs",jutils.array2stack(stack,128,128));
+			jutils.set_psize(imp,psize/1000.0f);
+			imp.show();
 		}
-		drawPoints(stack,spherepts,psize,psize,xystdev,zstdev,128,128);
-		ImagePlus imp=new ImagePlus("Sim NPCs",jutils.array2stack(stack,128,128));
-		jutils.set_psize(imp,psize/1000.0f);
-		imp.show();
 	}
 
-	public void plotPoints(float[][] coords){
+	public void addNoise(Object[] stack,float readstdev,float gain){
+		for(int i=0;i<stack.length;i++){
+			float[] pix=(float[])stack[i];
+			for(int j=0;j<pix.length;j++){
+				int photons=random.poidev((double)pix[j]);
+				float measured=gain*(float)photons;
+				measured+=random.gasdev(0.0,readstdev);
+				pix[j]=measured;
+			}
+		}
+	}
+
+	public void plotPoints(String name,float[][] coords){
 		float[][] xpts=new float[coords.length][1];
 		float[][] ypts=new float[coords.length][1];
 		float[][] zpts=new float[coords.length][1];
+		TextWindow tw=new TextWindow("Simulated Coordinates: "+name,"point\tx\ty\tz","",400,200);
 		for(int i=0;i<coords.length;i++){
 			xpts[i][0]=coords[i][0]/1000.0f;
 			ypts[i][0]=coords[i][1]/1000.0f;
 			zpts[i][0]=coords[i][2]/1000.0f;
+			tw.append(""+(i+1)+"\t"+xpts[i][0]+"\t"+ypts[i][0]+"\t"+zpts[i][0]);
 		}
 		Traj3D traj=new Traj3D("x","y","z",xpts,ypts,zpts,null);
 		int[] shapes=traj.getShapes();
 		for(int i=0;i<coords.length;i++){
 			shapes[i]=1;
 		}
-		new PlotWindow3D("Sphere Positions",traj).draw();
+		new PlotWindow3D("Sphere Positions: "+name,traj).draw();
 	}
 
 	public float[][] makeSphere(int nparticles,float radius,float xc, float yc, float zc){
 		float[][] coords=new float[nparticles][3];
-		rngs random=new rngs();
 		for(int i=0;i<nparticles;i++){
 			double[] dcoords=random.random_sphere(radius);
 			coords[i][0]=(float)dcoords[0]+xc;
@@ -73,7 +121,82 @@ public class simulate_npcs_jru_v1 implements PlugIn {
 		return coords;
 	}
 
-	public void drawPoints(Object[] stack,float[][] coords,float psize,float zsize,float xystdev,float zstdev,int width,int height){
+	public float[][] makeSpb(float bridgedist,float radius,float xc,float yc,float zc){
+		//this makes a mother and daughter spb with bridgedist between them
+		double[] dcoords=random.random_sphere(radius);
+		float[][] coords=new float[2][3];
+		coords[0][0]=(float)dcoords[0]+xc;
+		coords[0][1]=(float)dcoords[1]+yc;
+		coords[0][2]=(float)dcoords[2]+zc;
+		//now that we have the mother coordinate, we need to put the daughter in a random cone
+		//centered on the mother vector
+		//need to convert the bridge distance to an angle distance in radians
+		double angledist=Math.atan(bridgedist/radius);
+		//IJ.log(""+angledist);
+		double[] normvec={dcoords[0]/(double)radius,dcoords[1]/(double)radius,dcoords[2]/(double)radius};
+		//calculate a random cone vector
+		/*double[] conevec=random_cone_vector(normvec,angledist);
+		coords[1][0]=-(float)conevec[0]*radius+xc;
+		coords[1][1]=-(float)conevec[1]*radius+yc;
+		coords[1][2]=-(float)conevec[2]*radius+zc;*/
+		//alternatively, can always rotate about the vector z axis plane
+		/*double[] rotaxisvector=new double[3];
+		rotaxisvector[0]=normvec[1]; rotaxisvector[1]=-normvec[0];
+		double rval=Math.sqrt(rotaxisvector[0]*rotaxisvector[0]+rotaxisvector[1]*rotaxisvector[1]); //normalize the dot product
+		rotaxisvector[0]/=rval;
+		rotaxisvector[1]/=rval;*/
+		//or rotate about a random orthogonal vector
+		double[] randomvec=random.random_sphere(1.0);
+		double[] rotaxisvector=measure_object.crossProd(randomvec,normvec);
+		double rval=Math.sqrt(rotaxisvector[0]*rotaxisvector[0]+rotaxisvector[1]*rotaxisvector[1]+rotaxisvector[2]*rotaxisvector[2]);
+		rotaxisvector[0]/=rval;
+		rotaxisvector[1]/=rval;
+		rotaxisvector[2]/=rval;
+		rotate_vector(normvec,rotaxisvector,angledist);
+		coords[1][0]=(float)normvec[0]*radius+xc;
+		coords[1][1]=(float)normvec[1]*radius+yc;
+		coords[1][2]=(float)normvec[2]*radius+zc;
+		float spbdist=(float)Math.sqrt((coords[1][0]-coords[0][0])*(coords[1][0]-coords[0][0])+(coords[1][1]-coords[0][1])*(coords[1][1]-coords[0][1])+(coords[1][2]-coords[0][2])*(coords[1][2]-coords[0][2]));
+		//IJ.log("simulated spb dist "+spbdist);
+		return coords;
+	}
+
+	public double[] random_cone_vector(double[] center_vector,double theta){
+		//here theta is in radians: the half arc size of the cone
+		//first calculate the random cone vector about the z axis
+		double[] randvector=new double[3];
+		randvector[2]=Math.cos(theta);
+		double axialr=Math.sin(theta);
+		double angle=random.unidev(2.0*Math.PI,0.0); //this is the radial angle
+		randvector[0]=axialr*Math.cos(angle);
+		randvector[1]=axialr*Math.sin(angle);
+		//now find the vector orthogonal to the center_vector-zaxis plane (dot product)--have to normalize it
+		double[] rotaxisvector=new double[3];
+		rotaxisvector[0]=center_vector[1];
+		rotaxisvector[1]=-center_vector[0];
+		double rval=Math.sqrt(rotaxisvector[0]*rotaxisvector[0]+rotaxisvector[1]*rotaxisvector[1]);
+		rotaxisvector[0]/=rval;
+		rotaxisvector[1]/=rval;
+		//now find the angle between the z axis and the center vector (acos of dot product)
+		double rotangle=Math.acos(center_vector[2]);
+		//rotate the cone vector about the orthogonal vector
+		rotate_vector(randvector,rotaxisvector,rotangle);
+		return randvector;
+	}
+
+	public void rotate_vector(double[] invector,double[] rot_axis,double theta){
+		//this subroutine rotates a vector about rot_axis according to Rodrigues' formula
+		double xtemp,ytemp,ztemp,xrot,yrot,zrot,cos_theta,sin_theta;
+		cos_theta=Math.cos(theta);
+		sin_theta=Math.sin(theta);
+		xtemp=invector[0]; ytemp=invector[1]; ztemp=invector[2]; xrot=rot_axis[0]; yrot=rot_axis[1]; zrot=rot_axis[2];
+		invector[0]=((1.0-cos_theta)*xrot*xrot+cos_theta)*xtemp+((1-cos_theta)*yrot*xrot-zrot*sin_theta)*ytemp+((1.0-cos_theta)*zrot*xrot+yrot*sin_theta)*ztemp;
+		invector[1]=((1.0-cos_theta)*xrot*yrot+zrot*sin_theta)*xtemp+((1-cos_theta)*yrot*yrot+cos_theta)*ytemp+((1.0-cos_theta)*zrot*yrot-xrot*sin_theta)*ztemp;
+		invector[2]=((1.0-cos_theta)*xrot*zrot-yrot*sin_theta)*xtemp+((1-cos_theta)*yrot*zrot+xrot*sin_theta)*ytemp+((1.0-cos_theta)*zrot*zrot+cos_theta)*ztemp;
+		return;
+	}
+
+	public void drawPoints(Object[] stack,float[][] coords,float psize,float zsize,float xystdev,float zstdev,int width,int height,float amp){
 		gausfunc gf=new gausfunc();
 		float xystdevpix=xystdev/psize;
 		float zstdevpix=zstdev/zsize;
@@ -81,7 +204,7 @@ public class simulate_npcs_jru_v1 implements PlugIn {
 			float xpos=coords[i][0]/psize;
 			float ypos=coords[i][1]/psize;
 			float zpos=coords[i][2]/zsize;
-			gf.draw_3D_func(stack,xpos,ypos,zpos,width,height,xystdevpix,zstdevpix,1.0f);
+			gf.draw_3D_func(stack,xpos,ypos,zpos,width,height,xystdevpix,zstdevpix,amp);
 		}
 		return;
 	}
